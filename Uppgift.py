eb95b7d78413304996bda626e8ad449c92ad85bf
Goop0 <affe.gardebro@elev.ga.ntig.se>
2020-11-15 19:09:18 +0100

    debf

--- Uppgift.py
+++ Uppgift.py
@@ -1,13 +1,14 @@
 while True:
-    startvärde = int(input('skriv ett start värde '))
-    if startvärde <=0:
+    startvalue = int(input('write a start value '))
+    if startvalue <=0:
           
-        print('skriv ett tal som inte är negativt eller 0')
+        print('write a number that is not <=0')
         break
-    elif startvärde >0:
-        värde = startvärde
-        while startvärde <1000000:
-            startvärde * 2
-            dagar = startvärde/värde
-            dagar=int(dagar)
-        print(f'det tar {dagar} dagar')
+    elif startvalue >0.01:
+        value = startvalue
+        while startvalue <1000000:
+            startvalue * startvalue 
+
+            days = startvalue/value
+            days=int(days)
+            print(f'it takes {days} days')
